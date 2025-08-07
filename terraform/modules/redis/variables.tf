46f13a30bcd1510096567b604ad90b31216ba171
ancourn <unew5577@gmail.com>
2025-08-07 19:54:06 +0530

    Update terraform/modules/redis/variables.tf

--- terraform/modules/redis/variables.tf
+++ terraform/modules/redis/variables.tf
@@ -3,8 +3,49 @@ variable "environment" {
   type        = string
 }
 
-variable "project_name" {
-  description = "Project name"
+variable "cluster_id" {
+  description = "ElastiCache cluster ID"
+  type        = string
+}
+
+variable "node_type" {
+  description = "ElastiCache node type"
+  type        = string
+  default     = "cache.t3.medium"
+}
+
+variable "num_cache_nodes" {
+  description = "Number of cache nodes"
+  type        = number
+  default     = 2
+}
+
+variable "engine" {
+  description = "Cache engine"
+  type        = string
+  default     = "redis"
+}
+
+variable "engine_version" {
+  description = "Cache engine version"
+  type        = string
+  default     = "7.0"
+}
+
+variable "port" {
+  description = "Cache port"
+  type        = number
+  default     = 6379
+}
+
+variable "parameter_group_name" {
+  description = "Parameter group name"
+  type        = string
+  default     = "default.redis7"
+}
+
+variable "subnet_group_name" {
+  description = "Subnet group name"
   type        = string
 }
 
@@ -18,14 +59,65 @@ variable "private_subnets" {
   type        = list(string)
 }
 
-variable "security_group_id" {
-  description = "Redis security group ID"
-  type        = string
+variable "security_group_ids" {
+  description = "List of security group IDs"
+  type        = list(string)
+  default     = []
 }
 
-variable "kms_key_arn" {
-  description = "KMS key ARN for encryption"
+variable "at_rest_encryption_enabled" {
+  description = "Enable encryption at rest"
+  type        = bool
+  default     = true
+}
+
+variable "transit_encryption_enabled" {
+  description = "Enable encryption in transit"
+  type        = bool
+  default     = true
+}
+
+variable "auth_token" {
+  description = "Auth token for Redis"
   type        = string
+  default     = null
+  sensitive   = true
+}
+
+variable "automatic_failover_enabled" {
+  description = "Enable automatic failover"
+  type        = bool
+  default     = true
+}
+
+variable "multi_az_enabled" {
+  description = "Enable multi-AZ"
+  type        = bool
+  default     = true
+}
+
+variable "snapshot_retention_limit" {
+  description = "Number of days to retain snapshots"
+  type        = number
+  default     = 7
+}
+
+variable "snapshot_window" {
+  description = "Daily time range for snapshots"
+  type        = string
+  default     = "07:00-08:00"
+}
+
+variable "maintenance_window" {
+  description = "Weekly maintenance window"
+  type        = string
+  default     = "sun:08:00-sun:09:00"
+}
+
+variable "apply_immediately" {
+  description = "Apply changes immediately"
+  type        = bool
+  default     = false
 }
 
 variable "notification_topic_arn" {
@@ -34,15 +126,8 @@ variable "notification_topic_arn" {
   default     = null
 }
 
-variable "redis_password" {
-  description = "Redis password"
-  type        = string
-  sensitive   = true
-  default     = null
-}
-
 variable "tags" {
-  description = "Additional tags to apply to resources"
+  description = "A map of tags to assign to the resources"
   type        = map(string)
   default     = {}
 }
