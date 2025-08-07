6c48f84f8ea2271af34a0ae9f4e78683c18ae6c6
ancourn <unew5577@gmail.com>
2025-08-07 19:53:55 +0530

    Update terraform/modules/vpc/variables.tf

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,16 +1,17 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
 variable "environment" {
   description = "Environment name"
   type        = string
 }
 
-variable "project_name" {
-  description = "Project name"
-  type        = string
-}
-
 variable "vpc_cidr" {
-  description = "VPC CIDR block"
+  description = "CIDR block for VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "availability_zones" {
@@ -18,29 +19,29 @@ variable "availability_zones" {
   type        = list(string)
 }
 
-variable "private_subnets" {
-  description = "List of private subnet CIDR blocks"
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
   type        = list(string)
 }
 
-variable "public_subnets" {
-  description = "List of public subnet CIDR blocks"
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
   type        = list(string)
 }
 
 variable "database_subnets" {
-  description = "List of database subnet CIDR blocks"
+  description = "List of database subnet CIDRs"
   type        = list(string)
 }
 
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
+variable "enable_vpn_gateway" {
+  description = "Enable VPN gateway"
+  type        = bool
+  default     = false
 }
 
 variable "tags" {
-  description = "Additional tags to apply to resources"
+  description = "A map of tags to assign to the resources"
   type        = map(string)
   default     = {}
 }
